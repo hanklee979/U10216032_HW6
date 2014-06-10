8de37ae74ef953ff68e38f159f5369cbc7ceaf57
hanklee979 <hanklee979@gmail.com>
2014-06-10 20:29:14 +0800

    所有部分皆修改至main

--- src/BallControl.java
+++ src/BallControl.java
@@ -1,60 +1,52 @@
 import javax.swing.*;
+
 import java.awt.event.*;
 import java.awt.*;
+
 
 class BallControl extends JApplet {
 	
 	public static void main(String[] args){
-			
-		BallControl frame = new BallControl();
 		
 		Ball ball = new Ball();
 		JButton jbtSuspend = new JButton("Suspend");
 		JButton jbtResume = new JButton("Resume");
 		JScrollBar jsbDelay = new JScrollBar();		
 	    JPanel panel = new JPanel();
-    
-	    panel.add(jbtSuspend);
-        panel.add(jbtResume);		
-        
+	    	    
+	    
+        jsbDelay.setOrientation(JScrollBar.HORIZONTAL);  
         ball.setBorder(new javax.swing.border.LineBorder(Color.red));
-        jsbDelay.setOrientation(JScrollBar.HORIZONTAL);
-        
         ball.setDelay(jsbDelay.getMaximum());
-        
-		frame.setVisible(true); 	
-		
-	    jsbDelay.add(panel, BorderLayout.NORTH);		
-	    ball.add(panel, BorderLayout.CENTER);
-    	panel.add(panel, BorderLayout.SOUTH); 		
 
-	}
-	
-	public BallControl(){
-	    
-		setLayout(new BorderLayout());    	
-	    
-       
-    	
+        
+        ball.add(panel, BorderLayout.CENTER);        
+	    jsbDelay.add(panel, BorderLayout.NORTH);		
+	    panel.add(jbtSuspend);
+        panel.add(jbtResume);	
+ 		panel.setLayout(new BorderLayout());    
+ 		
     	jbtSuspend.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
-              ball.suspend();
+    			ball.suspend();
             }
         });
     	  
         jbtResume.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-              ball.resume();
+            	ball.resume();
             }
         });
           
         jsbDelay.addAdjustmentListener(new AdjustmentListener() {
             public void adjustmentValueChanged(AdjustmentEvent e) {
-              ball.setDelay(jsbDelay.getMaximum() - e.getValue());
+            	ball.setDelay(jsbDelay.getMaximum() - e.getValue());
             }
-        });    
-        
+        });            
+
+
 	}
+	
 }
 	
 	
