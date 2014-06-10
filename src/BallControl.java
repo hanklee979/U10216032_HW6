a734f9a1758feb87a2a9ba3b02718325d4001f93
hanklee979 <hanklee979@gmail.com>
2014-06-10 20:10:25 +0800

    修改main
    add panel部分改為application語法 放入main

--- src/BallControl.java
+++ src/BallControl.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-
 class BallControl extends JApplet {
 	
 	public static void main(String[] args){
-	
-	    JFrame frame = new JFrame();	
+			
+		BallControl frame = new BallControl();
 		
 		Ball ball = new Ball();
 		JButton jbtSuspend = new JButton("Suspend");
@@ -15,44 +14,47 @@ class BallControl extends JApplet {
 		JScrollBar jsbDelay = new JScrollBar();		
 	    JPanel panel = new JPanel();
     
-	
+	    panel.add(jbtSuspend);
+        panel.add(jbtResume);		
+        
+        ball.setBorder(new javax.swing.border.LineBorder(Color.red));
+        jsbDelay.setOrientation(JScrollBar.HORIZONTAL);
+        
+        ball.setDelay(jsbDelay.getMaximum());
+        
+		frame.setVisible(true); 	
+		
+	    jsbDelay.add(panel, BorderLayout.NORTH);		
+	    ball.add(panel, BorderLayout.CENTER);
+    	panel.add(panel, BorderLayout.SOUTH); 		
+
 	}
 	
-	
-	
-
-
-  public BallControl() {
-    // Group buttons in a panel
-	    panel.add(jbtSuspend);
-        panel.add(jbtResume);	
-
-
-    // Add ball and buttons to the panel
-    ball.setBorder(new javax.swing.border.LineBorder(Color.red));
-    jsbDelay.setOrientation(JScrollBar.HORIZONTAL);
-    ball.setDelay(jsbDelay.getMaximum());
-    setLayout(new BorderLayout());
-    add(jsbDelay, BorderLayout.NORTH);
-    add(ball, BorderLayout.CENTER);
-    add(panel, BorderLayout.SOUTH);
-
-    // Register listeners
-    jbtSuspend.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        ball.suspend();
-      }
-    });
-    jbtResume.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        ball.resume();
-      }
-    });
-    jsbDelay.addAdjustmentListener(new AdjustmentListener() {
-      public void adjustmentValueChanged(AdjustmentEvent e) {
-        ball.setDelay(jsbDelay.getMaximum() - e.getValue());
-      }
-    });
-  }
+	public BallControl(){
+	    
+		setLayout(new BorderLayout());    	
+	    
+       
+    	
+    	jbtSuspend.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+              ball.suspend();
+            }
+        });
+    	  
+        jbtResume.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              ball.resume();
+            }
+        });
+          
+        jsbDelay.addAdjustmentListener(new AdjustmentListener() {
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+              ball.setDelay(jsbDelay.getMaximum() - e.getValue());
+            }
+        });    
+        
+	}
 }
-
+	
+	
